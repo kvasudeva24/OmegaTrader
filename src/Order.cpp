2f0fb7ddce71ec063671e095fe23121b995df83a
kvasudeva24 <vasudevakartik@gmail.com>
2025-07-05 17:09:29 -0500

    Order implementation  done

---
+++ src/Order.cpp
@@ -0,0 +1,47 @@
+#include "Order.hpp"
+
+Order::Order(int i, int q, double p, Side s) : id(i), quantity(q), price(p), side(s) {}
+
+//make a new order after one already exists
+Order::Order(int i, const Order& other){
+    id = i;
+    quantity = other.getQuantity();
+    price = other.getPrice();
+    side = other.getSide();
+}
+
+//copy an order but keep the id the same after initialization
+Order& Order::operator=(const Order& other){
+    if(this != &other){
+        quantity = other.getQuantity();
+        price = other.getPrice();
+        side = other.getSide();
+    }
+    return *this;
+}
+
+int Order::getID() const {
+    return id;
+}
+
+int Order::getQuantity() const {
+    return quantity;
+}
+
+double Order::getPrice() const {
+    return price;
+}
+
+Side Order::getSide() const {
+    return side;
+}
+
+void Order::setQuantity(int q){
+    quantity = q;
+    return;
+}
+
+void Order::setPrice(double p){
+    price = p;
+    return;
+}
