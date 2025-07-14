1e5d90da6369832e19e17faf15ffc958cf3af02a
kvasudeva24 <vasudevakartik@gmail.com>
2025-07-14 11:34:24 -0500

    added hpp logic

--- includes/SMA.hpp
+++ includes/SMA.hpp
@@ -1,7 +1,26 @@
 //define a SMA strategy
 #include <Strategy.hpp>
+#include <queue>
 
 
 class SMA : public Strategy {
 
+private:
+    deque<double> sliding_window;
+    int size;
+    double moving_average;
+
+public:
+    SMA(int size);
+    SMA(const SMA& rhs);
+    SMA& operator=(const SMA& rhs);
+    virtual ~SMA() = default;
+
+    virtual void onMarketData(double price, int quantity, OrderBook& Ob) override;
+
+    double getMovingAverage() const;
+    void updateMovingAverage();
+
+    int getSize() const;
+    void setSize(int s);
 };
