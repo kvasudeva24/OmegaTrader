4abeda9be60f6c5447e0fa2780045f8b4fda6115
kvasudeva24 <vasudevakartik@gmail.com>
2025-07-25 14:15:07 -0500

    EMA test cases

--- tests/test_EMA.cpp
+++ tests/test_EMA.cpp
@@ -6,13 +6,25 @@
 #include <queue>
 
 
-OrderBook ob;
 
 
-TEST_CASE("Create EMA and test private member variables", "[WMA]"){
+TEST_CASE("Create EMA and test private member variables", "[EMA]"){
     EMA e1 = EMA(25);
     REQUIRE(e1.getMovingAverage() == 0.0);
     REQUIRE(e1.getSize() == 25);
     REQUIRE(e1.getSlidingWindow() == deque<double>());
     REQUIRE(e1.getK() == 2);
 }
+
+TEST_CASE("Moving average works correctly", "[EMA]"){
+    OrderBook ob;
+    EMA e1 = EMA(25);
+
+    e1.onMarketData(10.0, 1, ob);
+    REQUIRE(e1.getMovingAverage() == 10.0);
+    e1.onMarketData(20.0, 1, ob);
+    REQUIRE(e1.getMovingAverage() == 15.0);
+    e1.onMarketData(30.0, 1, ob);
+    REQUIRE(e1.getMovingAverage() == 22.5);
+}
+
